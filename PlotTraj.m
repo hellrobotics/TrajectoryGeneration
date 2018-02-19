952628afee186e750fbd1b319edb3cd8cd522cf0
Alex-Danielsen <alexada@stud.ntnu.no>
2018-02-19 22:34:13 +0100

    Added first attempt at switch trajectories. Fixed matlab visualization for 7239 robot dimensions (approximately).

--- PlotTraj.m
+++ PlotTraj.m
@@ -3,9 +3,24 @@ close all
 
 m2ft = 3.28084;
 ft2m = 1/m2ft;
+% table = readtable('traj.csv');
+filename = 'traj.csv';
+try
+    traj_out = csvread(filename);    
+catch
+    traj_out = csvread(filename,1,0);
+end
+    
+% if(isstring(table(,0)))
+%     traj_out = csvread('traj.csv',1,0);
+% else
+%     traj_out = csvread('traj.csv');
+% end
 
 %Read file
-traj_out = csvread('traj.csv');
+
+
+
 
 % Retreive values
 left_x     = traj_out(:,1);%*m2ft;
@@ -27,46 +42,100 @@ right_t    = traj_out(:,16);%zeros(size(right_dt));
 angle_deg  = traj_out(:,17);
 
 
+
 %Get time vector
 % for(i=[2:length(left_dt)])
 %     left_t(i) = left_t(i-1) + left_dt(i);
 %     right_t(i) = right_t(i-1) + right_dt(i);
 % end
 
+%draw back and front of robot at the beginning and end of the trajectory
+robotWidth  = ((33+5)/12)*ft2m; %m
+robotLength = ((28+5)/12)*ft2m; %m
+
+if(right_pos(end) < 0 && left_pos(end) < 0)
+    directionText = 'BACKWARD';
+    robotEndMult  = -1;
+else
+    directionText = 'FORWARD';
+    robotEndMult  = 1;
+end
+
+left_x0  = left_x(1)   -robotEndMult*(robotLength/2 * cosd(angle_deg(1)));
+left_y0  = left_y(1)   -robotEndMult*(robotLength/2 * sind(angle_deg(1)));
+right_x0 = right_x(1)  -robotEndMult*(robotLength/2 * cosd(angle_deg(1)));
+right_y0 = right_y(1)  -robotEndMult*(robotLength/2 * sind(angle_deg(1)));
+left_xf  = left_x(end) +robotEndMult*(robotLength/2 * cosd(angle_deg(end)));
+left_yf  = left_y(end) +robotEndMult*(robotLength/2 * sind(angle_deg(end)));
+right_xf = right_x(end)+robotEndMult*(robotLength/2 * cosd(angle_deg(end)));
+right_yf = right_y(end)+robotEndMult*(robotLength/2 * sind(angle_deg(end)));
+
+
 h1=figure();
-plot(left_x,left_y)
+plot([left_x0 left_x' left_xf],[left_y0 left_y' left_yf])
 hold on
-plot(right_x,right_y)
-xlim([0 54])
-ylim([-27 27])
-title("Path on Field")
-ylabel("Alliance station (feet)")
-xlabel("(feet)")
-legend('left', 'right')
-movegui(h1,'north')
+plot([right_x0 right_x' right_xf],[right_y0 right_y' right_yf])
+%diagonal corners
+plot(ft2m.*[0 2+(11.0/12)], ft2m.*[-11 -13.5])
+plot(ft2m.*[0 2+(11.0/12)], ft2m.*[11 13.5])
+%scale plates
+plot(ft2m.*[25,29,29,25,25], ft2m.*[4.5,4.5,7.5,7.5,4.5])
+plot(ft2m.*[25,29,29,25,25], ft2m.*-1*[4.5,4.5,7.5,7.5,4.5])
+%platform
+plot(ft2m.*[22,26.5,26.5,22,22],ft2m.*[5.5,5.5,-5.5,-5.5,5.5])
+%null territory
+plot(ft2m.*[24,30,30,24,24],ft2m.*[5.5625,5.5625,13.5,13.5,5.5625])
+plot(ft2m.*[24,30,30,24,24],ft2m.*-1*[5.5625,5.5625,13.5,13.5,5.5625])
+%switchplates
+plot(ft2m.*[12,16,16,12,12], ft2m.*[3,3,6,6,3])
+plot(ft2m.*[12,16,16,12,12], ft2m.*-1*[3,3,6,6,3])
+plot(ft2m.*[12,16,16,12,12], ft2m.*-1*[-6,-6,6,6,-6])
+plot(ft2m.*[16,17+(1.0/12),17+(1.0/12),16,16], ft2m.*[4+(11.0/12),4+(11.0/12),6,6,4+(11.0/12)])
+plot(ft2m.*[16,17+(1.0/12),17+(1.0/12),16,16], ft2m.*-1*[4+(11.0/12),4+(11.0/12),6,6,4+(11.0/12)])
+
+%exchange zone
+plot(ft2m.*[0,3,3,0,0],ft2m.*[1.25,1.25,5.25,5.25,1.25])
+
+%cube zone
+plot(ft2m.*[8.25,12,12,8.25,8.25], ft2m.*[-1.875,-1.875,1.875,1.875,-1.875])
+plot(ft2m.*[12-(1+(1.0/12)),12,12,12-(1+(1.0/12)),12-(1+(1.0/12))], ft2m.*[-(6.5/12),-(6.5/12),(6.5/12),(6.5/12),-(6.5/12)])
+plot(ft2m.*[12-(1+(1.0/12)),12,12,12-(1+(1.0/12)),12-(1+(1.0/12))], ft2m.*[(6.5/12),(6.5/12),(19.5/12),(19.5/12),(6.5/12)])
+plot(ft2m.*[12-(1+(1.0/12)),12,12,12-(1+(1.0/12)),12-(1+(1.0/12))], ft2m.*-1*[(6.5/12),(6.5/12),(19.5/12),(19.5/12),(6.5/12)])
+plot(ft2m.*[12-2*(1+(1.0/12)),12-(1+(1.0/12)),12-(1+(1.0/12)),12-2*(1+(1.0/12)),12-2*(1+(1.0/12))],ft2m.*[0,0,13.0/12,13.0/12,0])
+plot(ft2m.*[12-2*(1+(1.0/12)),12-(1+(1.0/12)),12-(1+(1.0/12)),12-2*(1+(1.0/12)),12-2*(1+(1.0/12))],ft2m.*[0,0,-13.0/12,-13.0/12,0])
+plot(ft2m.*[12-3*(1+(1.0/12)),12-2*(1+(1.0/12)),12-2*(1+(1.0/12)),12-3*(1+(1.0/12)),12-3*(1+(1.0/12))],ft2m.*[-(6.5/12),-(6.5/12),(6.5/12),(6.5/12),-(6.5/12)])
+
+% plot()
+xlim(ft2m.*[0 54])
+ylim(ft2m.*[-13.5 13.5])
+title({"Path on Field", strcat(directionText, " Trajectory")})
+ylabel("Alliance station (meters)")
+xlabel("(meters)")
+legend('left wheel (not bumper edge)', 'right wheel (not bumper edge)')
+pbaspect([2 1 1])
+movegui(h1,'west')
 
 h2=figure();
-subplot(4, 1, 1)
+subplot(5, 1, 1)
 plot(left_t, left_pos); hold on; plot(right_t, right_pos)
 ylabel('Position (ft)')
 title('Timed info')
 legend('left', 'right')
-subplot(4, 1, 2)
+subplot(5, 1, 2)
 plot(left_t, left_vel); hold on; plot(right_t, right_vel)
 ylabel('Velocity (ft/s)')
-subplot(4, 1, 3)
+subplot(5, 1, 3)
 plot(left_t, left_acc); hold on; plot(right_t, right_acc)
 ylabel('Acceleration (ft/s/s)')
-subplot(4, 1, 4)
+subplot(5, 1, 4)
 plot(left_t, left_jerk); hold on; plot(right_t, right_jerk)
 ylabel('Jerk (ft/s/s)')
 xlabel('Time (sec)');
-movegui(h2,'southeast')
+movegui(h2,'east')
 
-h3 = figure();
+% h3 = figure();
+subplot(5, 1, 5)
 plot(left_t, angle_deg)
 xlabel('Time (sec)');
 ylabel('Angle (deg)')
-movegui(h3,'southwest')
-
-
+% movegui(h3,'southwest')
--- src/TrajectoryGenerator.java
+++ src/TrajectoryGenerator.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Locale;
 
 import jaci.pathfinder.*;
 import jaci.pathfinder.modifiers.TankModifier;
@@ -10,22 +11,30 @@ public class TrajectoryGenerator {
 		final double M2FT = 3.28084;
 		final double FT2M = 1/M2FT;
 		
+		String filename = "traj.csv";
+		boolean wantHeaders   = false;
+		boolean backwardsTraj = false;
+		
+		double wheelbase_cm = 71.0; //cm
+        double wheelbase_m  = wheelbase_cm/100.0;
+        
 		//Trajectory Config Parms
 		final double TIME_STEP = 0.05; // Seconds
-		final double MAX_VEL = 4;    // ft/s
-		final double MAX_ACC = 10.0;    // ft/s/s
-		final double MAX_JERK = 60.0;  // ft/s/s/s
+		final double MAX_VEL = 1.5;    // m/s
+		final double MAX_ACC = 1;    // m/s/s
+		final double MAX_JERK = 6.0;  // m/s/s/s
 		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, TIME_STEP, MAX_VEL, MAX_ACC, MAX_JERK);
         Waypoint[] points = new Waypoint[] {
 //        		new Waypoint(0,0,0),
 //        		new Waypoint(10, 0, 0)
         		
         		
-                new Waypoint(0, 0, 0),
-                new Waypoint(8, 0, Pathfinder.d2r(0)),
-                new Waypoint(12, -4, Pathfinder.d2r(-90)),
-                new Waypoint(12, -15, Pathfinder.d2r(-90)),              
-                new Waypoint(14, -17, Pathfinder.d2r(0))    
+                new Waypoint(((33.0/12.0)*FT2M/2.0), -.127, 0),
+                new Waypoint(3.2, 1.2, Pathfinder.d2r(10)),
+//                new Waypoint(3.2, -1.2, Pathfinder.d2r(-10)),
+//                new Waypoint(12, -4, Pathfinder.d2r(-90)),
+//                new Waypoint(12, -15, Pathfinder.d2r(-90)),              
+//                new Waypoint(14, -17, Pathfinder.d2r(0))    
                 
 //                new Waypoint(.5, 0, 0),
 //                new Waypoint((2.58+2.1)/2, 3.64/2, Pathfinder.d2r(-45)),
@@ -34,14 +43,26 @@ public class TrajectoryGenerator {
 
         Trajectory trajectory = Pathfinder.generate(points, config);
 
-        // Wheelbase Width = 0.5m
-        double wheelbase_in = 27; //in
-        double wheelbase_ft  = wheelbase_in/12;
-        TankModifier modifier = new TankModifier(trajectory).modify(wheelbase_ft);
+        TankModifier modifier = new TankModifier(trajectory).modify(wheelbase_m);
 
         // Do something with the new Trajectories...
         Trajectory left = modifier.getLeftTrajectory();
         Trajectory right = modifier.getRightTrajectory();
+        
+        if(backwardsTraj) {
+        	//flip left and right
+            right = modifier.getLeftTrajectory();
+            left = modifier.getRightTrajectory();
+        	//negate everything
+            
+        	//add 180 deg to angle (might solve itself with the flipping of left and right
+        }
+        else {
+        	// Do something with the new Trajectories...
+            left = modifier.getLeftTrajectory();
+            right = modifier.getRightTrajectory();
+        }
+        
         System.out.println("Done");
         System.out.println("Left segment length: "  + left.length());        
         System.out.println("Right segment length: " + right.length());
@@ -54,34 +75,44 @@ public class TrajectoryGenerator {
         double[] left_t    = new double[left.length()];
         double[] right_t   = new double[right.length()];
         double[] angle_deg = new double[left.length()];
-        left_t[0] = 0; right_t[0] = 0; angle_deg[0] = 0;
+        left_t[0] = 0; 
+        right_t[0] = 0;
+    	angle_deg[0] = Pathfinder.r2d(Math.atan2(left.get(0).y-right.get(0).y, left.get(0).x-right.get(0).x))-90;
         for(int i = 1; i < left.length(); i++) {
         	left_t[i] = left_t[i-1] + left.get(i-1).dt;
         	right_t[i] = right_t[i-1] + right.get(i-1).dt;
-        	double y2 = trajectory.get(i).y;
-        	double y1 = trajectory.get(i-1).y;
-        	double x2 = trajectory.get(i).x;
-        	double x1 = trajectory.get(i-1).x;
+        	double y2 = left.get(i).y;
+        	double y1 = right.get(i).y;
+        	double x2 = left.get(i).x;
+        	double x1 = right.get(i).x;
         	
-        	angle_deg[i] = Pathfinder.r2d(Math.atan2(y2-y1, x2-x1));
+        	angle_deg[i] = Pathfinder.r2d(Math.atan2(y2-y1, x2-x1))-90;
         }
 //        for(int i = 1; i < right.length(); i++) {
 //        	right_t[i] = right_t[i-1] + right.get(i-1).dt;
 //        }
 
         
-        try(PrintWriter out = new PrintWriter("traj.csv")) {
-	        for(int i =0; i < left.length(); i ++) {
-	        	out.printf("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", 
-	        			left.get(i).x, left.get(i).y, 
-	        			right.get(i).x, right.get(i).y, 
-	        			left.get(i).position, right.get(i).position,
-	        			left.get(i).velocity, right.get(i).velocity,
-	        			left.get(i).acceleration, right.get(i).acceleration,
-	        			left.get(i).jerk, right.get(i).jerk,
-	        			left.get(i).dt, right.get(i).dt,
-	        			left_t[i],right_t[i],
-	        			angle_deg[i]);
+        try(PrintWriter out = new PrintWriter(filename)) {
+        	if(wantHeaders) {
+        		out.printf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
+        				"leftX","leftY","rightX","rightY","leftPos","rightPos","leftVel","rightVel",
+        				"leftAcc","rightAcc","leftJerk","rightJerk","leftDt","rightDt",
+        				"leftTime","rightTime","angle"
+        				);
+        	}
+        	int backMult = backwardsTraj ? -1 : 1;
+        	for(int i =0; i < left.length(); i ++) {
+	        	out.printf(String.format(Locale.US,"%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", 
+	        			left.get(i).x,                     left.get(i).y, 
+	        			right.get(i).x,                    right.get(i).y, 
+	        			backMult*left.get(i).position,     backMult*right.get(i).position,
+	        			backMult*left.get(i).velocity,     backMult*right.get(i).velocity,
+	        			backMult*left.get(i).acceleration, backMult*right.get(i).acceleration,
+	        			backMult*left.get(i).jerk,         backMult*right.get(i).jerk,
+	        			left.get(i).dt,                    right.get(i).dt,
+	        			left_t[i],                         right_t[i],
+	        			angle_deg[i]));
 	        }
         } catch (java.io.FileNotFoundException e) {
         	
